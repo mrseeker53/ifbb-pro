b3d8d1f419f754a04655caea3cd8ba2906bd5509
mrseeker53 <mi.nayeem30@gmail.com>
2022-09-29 19:48:30 +0600

    display workout data

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -3,11 +3,17 @@ import './Workout.css'
 
 const Workout = (props) => {
     const { workout } = props;
-    const { picture, name, description } = workout;
+    const { picture, name, description, time } = workout;
 
     return (
-        <div>
-            
+        <div className='workout'>
+            <img src={picture} alt="" />
+            <div className='workout-detail'>
+                <p>{name}</p>
+                <p><small>{description}</small></p>
+                <p>Time: {time}s</p>
+            </div>
+            <button>Add to build</button>
         </div>
 
     );
