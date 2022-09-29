1b4f34460b6c8d186a577c401c9a6a0f0e104f70
mrseeker53 <mi.nayeem30@gmail.com>
2022-09-29 20:15:43 +0600

    updated workout

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './Workout.css'
 
 const Workout = (props) => {
-    const { workout } = props;
+    const { workout, handleAddToBuild } = props;
     const { picture, name, description, time } = workout;
 
     return (
         <div className='workout'>
             <img src={picture} alt="" />
             <div className='workout-detail'>
-                <p>{name}</p>
+                <p className='workout-name'>{name}</p>
                 <p><small>{description}</small></p>
                 <p>Time: {time}s</p>
             </div>
-            <button>Add to build</button>
+            <button onClick={() => handleAddToBuild(workout)} className='btn-build'>Add to build</button>
         </div>
 
     );
